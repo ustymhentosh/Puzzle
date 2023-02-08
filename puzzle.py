c37d07a14a83952fc82dd29977a938723d8005a2
Ustym Hentosh <116552529+ustymhentosh@users.noreply.github.com>
2023-02-08 11:04:08 +0200

    Update puzzle.py
    create function distribution

--- puzzle.py
+++ puzzle.py
@@ -1,1 +1,26 @@
 # Puzzle
+
+
+def validate_rows(board):
+  pass
+
+
+
+
+
+def validate_columns(board):
+  pass
+
+
+
+
+
+def validate_colour(board):
+  pass
+
+
+
+
+
+def validate_board(board):
+  pass
