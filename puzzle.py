08f60a06db39ab77ba26090f98fc289decc170d4
UkrKreuzritter <116711489+UkrKreuzritter@users.noreply.github.com>
2023-02-08 12:50:23 +0200

    columns +

--- puzzle.py
+++ puzzle.py
@@ -9,7 +9,20 @@ def validate_rows(board):
 
 
 def validate_columns(board):
-  pass
+    """
+    Checks columns
+    >>> validate_columns(["**** ****", "***1 ****", "**  3****", "* 4 1****","     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    False
+    """
+    for i in range (9):
+        newstring=""
+        for k in range(9):
+            if board[k][i]!=' ' or board[k][i]!='*':
+                newstring+=board[k][i]
+        for k in range(1,10):
+            if newstring.count(str(k))>1:
+                return False
+    return True
 
 
 
