602128756fa41455897c1c08943895f433dff9dd
Ustym Hentosh <116552529+ustymhentosh@users.noreply.github.com>
2023-02-08 18:55:25 +0200

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -1,8 +1,26 @@
 # Puzzle
 
 
-def validate_rows(board):
-  pass
+def validate_row(board):
+    """
+    Check rows
+    >>> validate_row(["**** ****","***1 ****","**  3****","* 4 1****","     9 5 "," 6  83  *","3   1  **","  8  2***","  2  ****"])
+    True
+    """
+    
+    
+    a = []
+    for line in board:
+        for x, j in enumerate (line):
+            if j != '*' and j != ' ':
+                a.append(j)
+                
+            for i in range(len(a)-1):
+                for j in range(i+1, len(a)):
+                    if a[i] ==a[j]:
+                        return False
+        a = []
+    return True
 
 
 
